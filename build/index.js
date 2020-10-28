5fc752afc356992f257f293e26353aee4a4adef6
Paola Garay <garay.angela@gmail.com>
2020-10-28 11:47:07 +0100

    Refactor adding abstract classes

--- build/index.js
+++ build/index.js
@@ -1,12 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
 var CharactersCollection_1 = require("./CharactersCollection");
-// const numbersCollection = new NumbersCollection([-50, 8, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// const numbersCollection = new NumbersCollection([-50, 8, -5, 0, 7, -1]);
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
-var charactersCollection = new CharactersCollection_1.CharactersCollection('paolaGaraY');
-var sorter = new Sorter_1.Sorter(charactersCollection);
-sorter.sort();
+var charactersCollection = new CharactersCollection_1.CharactersCollection('XaatyfgrWQPgs');
+charactersCollection.sort();
 console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,10 @@ import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 
-// const numbersCollection = new NumbersCollection([-50, 8, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// const numbersCollection = new NumbersCollection([-50, 8, -5, 0, 7, -1]);
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
-
-
-const charactersCollection = new CharactersCollection('paolaGaraY');
-const sorter = new Sorter(charactersCollection );
-sorter.sort();
+const charactersCollection = new CharactersCollection('XaatyfgrWQPgs');
+charactersCollection.sort();
 console.log(charactersCollection.data);
